6302c75dcf4527ab3774ce1ceb5c310ed50153ba
GalaArITT <oljagalavizar@ittepic.edu.mx>
2021-11-17 07:26:15 -0700

    Ejemplo de ag-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ag-grid-child';
+  @ViewChild('agGrid') agGrid: AgGridAngular;
+
+  columnDefs: ColDef[] = [
+    {field: 'make', rowGroup:true},
+    {field: 'price' }
+  ];
+  
+  defaultColDef: ColDef = {
+    sortable: true,
+    filter: true
+  };
+  
+  autoGroupColumnDef: ColDef = {
+    headerName: 'Model',
+    field: 'model',
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+        checkbox: true
+    }
+  };
+
+  rowData:Observable<any>; 
+  constructor(private http: HttpClient){
+    this.rowData = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  }
+  getSelectedRows(): void {
+      const selectedNodes = this.agGrid.api.getSelectedNodes();
+      const selectedData = selectedNodes.map(node => node.data);
+      const selectedDataStringPresentation = selectedData.map(node => `${node.make} ${node.model}`).join(', ');
+      alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
 }
